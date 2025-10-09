09c49b712ee80d34bd32c6539021a7962e5ce1a1
Khuong Anh Kiet <teaz.khuonganhkiet@gmail.com>
2025-10-09 11:44:56 +0700

    testing blog view mode

--- frontend/src/data/placeholderData.tsx
+++ frontend/src/data/placeholderData.tsx
@@ -4,7 +4,6 @@ export const placeholderData: BlogItemProps[] = [
     {
         id: '1',
         title: 'Coming soon',
-        slug: 'coming-soon',
         description: 'Coming soon',
         coverImage: 'placeholder',
         timeStamp: '2023-10-01T12:00:00+07:00',
@@ -22,7 +21,6 @@ export const placeholderData: BlogItemProps[] = [
     {
         id: '2',
         title: 'Coming soon2',
-        slug: 'coming-soon2',
         description: 'Coming soon2',
         coverImage: 'placeholder',
         timeStamp: '2023-09-15T08:30:00+07:00',
@@ -30,7 +28,6 @@ export const placeholderData: BlogItemProps[] = [
     {
         id: '3',
         title: 'Coming soon3',
-        slug: 'coming-soon3',
         description: 'Coming soon3',
         coverImage: 'placeholder',
         timeStamp: '2023-08-20T14:45:00+07:00',
